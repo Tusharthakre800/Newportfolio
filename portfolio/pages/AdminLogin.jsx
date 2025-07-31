f6ecdb8e3899c6bd4230b6dcf0a65c09b9a4efc2
tushar <tusharthakre800@gmail.com>
2025-08-01 00:59:20 +0530

    new url

--- portfolio/pages/AdminLogin.jsx
+++ portfolio/pages/AdminLogin.jsx
@@ -8,7 +8,7 @@ const AdminLogin = ({ onLoggedIn }) => {
   const submit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/admin-auth/login", {
+      const res = await axios.post("https://api.tusharwebdev.online/admin-auth/login", {
         username: form.username,
         password: form.password,
       });
